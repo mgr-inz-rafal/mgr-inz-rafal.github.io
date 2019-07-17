7b7fdb313d399fff457980701661946911a0ee4b
mgr-inz-rafal <rchabowski@gmail.com>
2019-07-17 21:52:43 +0200

    Docs update

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["swarm_it"] = {"name":"","dirs":[],"files":["carrier.rs","dispatcher.rs","lib.rs","macros.rs","payload.rs","position.rs","slot.rs"]};
+sourcesIndex["swarm_it"] = {"name":"","files":["carrier.rs","dispatcher.rs","lib.rs","macros.rs","payload.rs","position.rs","slot.rs","tools.rs"]};
 createSourceSidebar();
